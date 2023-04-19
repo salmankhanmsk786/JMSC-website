0dbd86038a1daae20197aeb4aeefdc5d687dc963
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-04-18 17:16:40 -0700

    Add files via upload

---
+++ jmsc-website/src/Subtotal.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+function Subtotal() {
+  const [{ cart }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Part of the hw */}
+              Subtotal ({cart.length} items):
+              <strong>{value}</strong>
+            </p>
+            <small className="subtotal_gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(cart)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
