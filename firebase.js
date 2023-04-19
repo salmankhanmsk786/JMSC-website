ced1ddca2fbe7e2e7613795844762acbb2f5faa9
hcm1164 <64693176+hcm1164@users.noreply.github.com>
2023-04-18 21:10:46 -0700

    Checkout and customer rating.
    my version of the Checkout and customer rating pages.

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8PiNyoMVrk7unAIurAMHJQDS6zwBrg6c",
+    authDomain: "clone-d1a91.firebaseapp.com",
+    projectId: "clone-d1a91",
+    storageBucket: "clone-d1a91.appspot.com",
+    messagingSenderId: "236388937381",
+    appId: "1:236388937381:web:6fc9ae825190e3d5d6274c",
+    measurementId: "G-6L6TVDB4L2"
+  };
