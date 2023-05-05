50d64e21fb2b25ea9b47a164e3e06417b57fae44
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-05-05 15:44:26 -0700

    Update Product.js

--- jmsc-website/src/Product.js
+++ jmsc-website/src/Product.js
@@ -1,9 +1,21 @@
+/*
+  4/10/2023
+  Salman Khan, Mauricio Gonzalez
+  Joshua Santillan, Cheol Han
+  This code pertains to the actual items being sold
+  on our website.
+  This file contains the actual item object class constructor
+  that contains the fields that will be shown
+   on our website such as its name, a picture, price, etc.
+  Also has the code for a button to be implemented underneath the picture
+  of the item which will add that item to customers cart
+ */
 import React from 'react'
 import "./Product.css";
 import { useStateValue} from "./StateProvider";
 
 function Product({ id, title, image, price, rating}) {
-    const [{cart}, dispatch] = useStateValue();
+  const [{cart}, dispatch] = useStateValue();
 
   console.log("this is the cart >>> ", cart);
     const addToCart = () => {
