27d2a6909167cd2a10c3023de8f16080576efb1f
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-05-05 15:38:53 -0700

    Update Checkout.js

--- jmsc-website/src/Checkout.js
+++ jmsc-website/src/Checkout.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import "./Checkout.css"
+/*Salman Khan, Mauricio Gonzalez
+4/10/2023
+This code implements what the customer will see 
+when in the checkout page
+cart.map is the function that keeps track of the items
+in the cart as an array
+*/ 
+import React from "react";
+import "./Checkout.css";
 import Subtotal from "./Subtotal";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 
+
 function Checkout() {
-  const [{cart}, dispatch] = useStateValue();
+  const [{cart, user}, dispatch] = useStateValue();
+
   return (
-    <div className='checkout'>
+    <div className="checkout">
+      <div className="checkout_left">
+        <div className="checkout_title">
+          <h3>Hello, {user?.email}</h3>
+          <h2>Your shopping Cart</h2>
 
-        <div className='checkout_left'>
-
-            <div className='checkout_title'>
-    
-                <h2>Your shopping Cart</h2>
-    
-                         {cart.map(item => (
+          {cart.map(item => (
             <CheckoutProduct 
             id ={item.id}
               title={item.title}
@@ -25,21 +32,17 @@ function Checkout() {
             />
           ) )}
 
-            </div>
+
 
         </div>
+      </div>
 
-
-        <div className='checkout_right'>
-            <h2>Your subtotal : </h2>
-        
-        </div>
-
-
-
-
+      <div className="checkout_right">
+        <Subtotal />
+        <h2>Your subtotal : </h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
