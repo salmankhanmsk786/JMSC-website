fead2bd7f4c267a637031ff83c3683c843de94f1
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-05-05 15:41:12 -0700

    Update Firebase.js

--- jmsc-website/src/Firebase.js
+++ jmsc-website/src/Firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 const firebaseConfig = {
     apiKey: "AIzaSyCyWGAFAQTRsEIgNk-KDz5K1N_ZMdMa93I",
     authDomain: "jmsc-website.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:738678569522:web:d94d5481dfe30d2b02da2b",
     measurementId: "G-DZ1MXSS7XP"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
