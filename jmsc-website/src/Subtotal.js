1beb407d41f314dc3c99e717c057d88ea8bce687
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-05-05 15:45:01 -0700

    Update Subtotal.js

--- jmsc-website/src/Subtotal.js
+++ jmsc-website/src/Subtotal.js
@@ -1,3 +1,18 @@
+/*
+4/14/2023
+Mauricio Gonzalez, Salman Khan,
+Cheol Han, Joshua Santillan
+Code that calculates the toal owed from the customer from adding the
+price of all the items in the cart
+Implements the cart array used in other files so that this file knows what the items cost
+are and how to add it all together. 
+Also includes the proceed to checkout button that the customer will use when the 
+customer is done shopping. This is also where we use a library from react
+that knows how to implement certian aspects of actual real world currency.
+This is important as real world currency can get tricky when trying to implement 
+into code
+
+*/
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
