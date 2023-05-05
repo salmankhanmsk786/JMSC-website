07eb283e14b753b5320e9c987a49ed6a41ca962f
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-05-05 15:38:07 -0700

    Update App.js

--- jmsc-website/src/App.js
+++ jmsc-website/src/App.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-import { BrowserRouter as Router, Route , Routes }
-from "react-router-dom";
-import Checkout from './Checkout';
-
-
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run one when the app component loads...
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({  
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
   return (
-    
     <Router>
-      
       <div className="app">
-
-        <Header/>
-    
-    
+      <Header />
+      
         <Routes>
-          <Route path="/checkout" element={[ <Checkout/>]}/>
-          <Route path="/" element={[<Home />]}/>
+        <Route path = "/login" element = {[< Login />]} />
+          <Route path="/checkout" element={[<Checkout />]} />
+          
+          <Route path="/"  element={[<Home />]} />
+          
         </Routes>
-    
       </div>
-
     </Router>
-
   );
 }
 
