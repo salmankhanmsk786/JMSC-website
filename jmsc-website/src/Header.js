248866d8a242f241b8bf2775caeb4bf6cfc2a2cb
mgonz164 <125494421+mgonz164@users.noreply.github.com>
2023-05-05 15:41:40 -0700

    Update Header.js

--- jmsc-website/src/Header.js
+++ jmsc-website/src/Header.js
@@ -1,13 +1,30 @@
+/*
+4/3/2023
+Salman Khan
+This code is for the header of the website. Includes
+our own logo, and the items we wish to sell to customers
+The images of our items will be in the public folder under assets
+Includes the button to go to cart when customer is done shopping
+and the search bar when customer wants to look for a specific item.
+Login can also be accessed here
+
+ */
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import { Link } from "react-router-dom"
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
-    const [{cart}, dispatch] = useStateValue();
 
+  const [{cart, user}, dispatch] = useStateValue();
+  
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
   return (
     <div className = "header">
 
@@ -29,31 +46,37 @@ function Header() {
 
         </div>
         <div className="header_nav">
-            <div className="header_option">
+            <Link to = {!user && "/login"}>
+            <div onClick = {handleAuthentication}
+            className="header_option">
 
             <span className='header_optionlineone'> Hello Guest </span>
-            <span className='header_optionlineone'> Sign in</span>
+            <span className='header_optionlineTwo'> 
+            {user ? 'Sign Out' : 'Sign In'}</span>
             
             </div>
+            </Link>
 
             <div className="header_option">
 
             <span className='header_optionlineone'> Return </span>
-            <span className='header_optionlineone'> & Orders</span>
+            <span className='header_optionlineTwo'> & Orders</span>
                 
             </div>
             
             <div className="header_option">
 
             <span className='header_optionlineone'> Your </span>
-            <span className='header_optionlineone'> JMSC</span>
+            <span className='header_optionlineTwo'> JMSC</span>
                 
             </div>
 
             <Link to="/checkout">
             <div className = "header_optionBasket">
             <ShoppingBagIcon />
-            <span className="header_optionLineTwo header_basketCount" > {cart?.length}  </span>
+            <span className="header_optionLineTwo header_basketCount" > 
+            {cart?.length} 
+            </span>
             </div>
             
             </Link>
@@ -63,5 +86,5 @@ function Header() {
     </div>
   )
 }
-
+}
 export default Header
